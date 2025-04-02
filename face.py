0b0bea9f127a9ed43422819354996629efe91543
Genrey <109770981+Genrei123@users.noreply.github.com>
2025-04-02 21:02:51 +0800

    For presentation

--- face.py
+++ face.py
@@ -21,8 +21,8 @@ mp_face_mesh = mp.solutions.face_mesh
 face_mesh = mp_face_mesh.FaceMesh(
     max_num_faces=1,
     refine_landmarks=True,
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5
+    min_detection_confidence=0.3,
+    min_tracking_confidence=0.3
 )
 mp_drawing = mp.solutions.drawing_utils
 
